2cadbace84bb1b6a5b8f147a075f992bf4e87bb9
Beirens Gregory <gregory.beirens@gmail.com>
2018-03-08 13:33:06 +0100

    build commit 0.0.3

--- dist/xAssist-object.js
+++ dist/xAssist-object.js
@@ -1,6 +1,6 @@
-// https://github.com/GregBee2/xassist-object#readme Version 0.0.2.
+// https://github.com/GregBee2/xassist-object#readme Version 0.0.3.
 // Copyright 2018 undefined.
-// Created on Thu, 08 Mar 2018 12:25:23 GMT.
+// Created on Thu, 08 Mar 2018 12:33:02 GMT.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
