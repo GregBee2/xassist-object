79150afd838abcdc2ff7dbb11c1d351997da3097
Beirens Gregory <gregory.beirens@gmail.com>
2018-03-08 13:10:27 +0100

    $npm_package_version

--- dist/xAssist-object.js
+++ dist/xAssist-object.js
@@ -1,6 +1,6 @@
 // https://github.com/GregBee2/xassist-object#readme Version 0.0.1.
 // Copyright 2018 undefined.
-// Created on Thu, 08 Mar 2018 12:07:21 GMT.
+// Created on Thu, 08 Mar 2018 12:10:25 GMT.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
