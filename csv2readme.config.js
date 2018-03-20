cd08a4399d890788b0589cc1028a5c633ec5ae17
gregbee2 <gregory.beirens@gmail.com>
2018-03-20 09:04:06 +0100

    introduced readme2csv

---
+++ csv2readme.config.js
@@ -0,0 +1,41 @@
+var csv2readme = require('csv2readme');
+const definition = require("./package.json");
+
+var options={
+	input:{
+		base:"../../helpData/csv/base.csv",
+		functionParam:"../../helpData/csv/functionParams.csv",
+		classDef:"../../helpData/csv/classes.csv"
+	},
+	moduleName:"xassist-object",
+	globalTOC:false,
+	header:{
+		title:"@xassist/xassist-object",
+		explanation:["This module adds different methods to the generic javascript `Object`.",
+			"It does not extend the javascript `Object` itself, but defines a subClass."].join("\r\n")
+	},
+	headerFiles:["../../helpData/markdown/installationModule.md"],
+	includeDependencies:true,
+	includeLicense:true,
+	footerFiles:[/*"dependencies.md","src/license.md"*/],
+	subTitle:"API",
+	output:{
+		file:"README.md"
+	},
+	baseLevel:3,
+	headerTemplates:{
+		moduleName:"xassist-object",
+		moduleUrl:"https://raw.githubusercontent.com/GregBee2/xassist-object/master/dist/xAssist-object.min.js",
+		libraryName:"xassist",
+		libraryUrl:"https://github.com/GregBee2/xassist",
+		moduleTest:"object()"
+	},
+	footerTemplates:{
+		/*license:definition.license,
+		licenseUrl:"https://choosealicense.com/licenses/"+definition.license.toLowerCase()*/
+	}
+};
+csv2readme.init(options);
+
+	
+	
